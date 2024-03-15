3e2d1b5ffc0b2a1080e6cf44fcb8d2aefa050323
Njyon <thenjyon@googlemail.com>
2024-03-15 15:41:29 +0100

    Mino AI + AI Condition stuff

--- Assets/Logic/AI/BTDecorators/BTTargetDistanceCondition.cs
+++ Assets/Logic/AI/BTDecorators/BTTargetDistanceCondition.cs
@@ -1,28 +1,44 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 
+public enum EConditionCheck
+{
+	Or,
+	And,
+}
+
 [Category("Condition")]
 public class BTTargetDistanceCondition : BTHyppoliteConditionDecoratorBase
 {
-	[Header("TargetDistanceCondition")]
-	public float minDistance = 1f;
-	public float treshhold = 1f;
-	public bool checkWithHight = false;
-	public bool inVerseCheck = false;
+	public EConditionCheck conditonCheck = EConditionCheck.And;
+	public List<ClassInstance<ConditionOperation>> conditions = new List<ClassInstance<ConditionOperation>>();
 
 	protected override bool OnCheckCondition(object options = null)
 	{
-		float distance;
-		if (checkWithHight)
-			distance = Vector3.Distance(GameCharacter.transform.position, TargetGameCharacter.transform.position);
-		else
-			distance = Vector3.Distance(Ultra.Utilities.IgnoreAxis(GameCharacter.transform.position, EAxis.YZ), Ultra.Utilities.IgnoreAxis(TargetGameCharacter.transform.position, EAxis.YZ));
+		bool result = false;
+		foreach (ClassInstance<ConditionOperation> condition in conditions)
+		{
+			condition.instance.Init(GameCharacter, TargetGameCharacter);
+			result = condition.instance.DoOperation();
 
-		if (inVerseCheck)
-			return !Ultra.Utilities.IsNearlyEqual(distance, minDistance, treshhold);
-		else
-			return Ultra.Utilities.IsNearlyEqual(distance, minDistance, treshhold);
+			switch (conditonCheck)
+			{
+				case EConditionCheck.Or:
+					if (result)
+						return true;
+					break;
+				case EConditionCheck.And:
+					if (!result) 
+						return false;
+					break;
+				default: break;
+			}
+
+		}
+		return result;
 	}
 }
---
+++ Assets/Logic/AI/BTDecorators/ConditionOperation/ConditionGreaterOperation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionGreaterOperation : ConditionOperation
+{
+	[Header("TargetDistanceCondition")]
+	public bool greaterEqualOperation = false;
+	public float checkDistance = 5f;
+	public bool checkWithHight = false;
+	public bool inverseCheck = false;
+
+	public override bool DoOperation()
+	{
+		float distance;
+		if (checkWithHight)
+			distance = Vector3.Distance(GameCharacter.transform.position, Target.transform.position);
+		else
+			distance = Vector3.Distance(GameCharacter.transform.position.IgnoreAxis(EAxis.YZ), Target.transform.position.IgnoreAxis(EAxis.YZ));
+
+		if (inverseCheck)
+			return !(greaterEqualOperation ? distance >= checkDistance : distance > checkDistance);
+		else
+			return (greaterEqualOperation ? distance >= checkDistance : distance > checkDistance);
+	}
+}
