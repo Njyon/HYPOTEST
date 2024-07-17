f8aac0c7c3101dba5c9bf8b1ab9e16bf28ddbe21
Njyon <thenjyon@googlemail.com>
2024-07-17 17:03:36 +0200

    Update HoldAttackWithKickAway.cs

--- Assets/Logic/Code/Weapons/Attacks/Actions/Attacks/HoldAttackWithKickAway.cs
+++ Assets/Logic/Code/Weapons/Attacks/Actions/Attacks/HoldAttackWithKickAway.cs
@@ -16,7 +16,34 @@ public class HoldAttackWithKickAway : AttackBase
 
 	public override void StartAction()
 	{
+		StartAttack(attackData.attackAnimation);
+	}
 
+	public override void TriggerAnimationEvent()
+	{
+		GameCharacter.AnimController.SetHoldAttack(attackData.holdAttackAnimation);
+		GameCharacter.AnimController.InAttack = false;
+		GameCharacter.AnimController.HoldAttack = true;
+	}
+
+	public override void PostAttackStateLogic(float deltaTime)
+	{
+		if (GameCharacter.PluginStateMachine.ContainsPluginState(EPluginCharacterState.HoldAttack))
+		{
+			// TODO Move to Enemy
+		}else
+		{
+			ActionInterupted();
+			return;
+		}
+	}
+
+	public override void ActionInterupted()
+	{
+		GameCharacter.AnimController.InAttack = false;
+		GameCharacter.AnimController.HoldAttack = false;
+		GameCharacter.CombatComponent.CurrentWeapon.HitDetectionEnd();
+		GameCharacter.StateMachine.RequestStateChange(EGameCharacterState.AttackRecovery);
 	}
 
 	public override ActionBase CreateCopy()
